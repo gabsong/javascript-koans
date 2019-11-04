e218b2ea6d5c6abae693199ab7a8a4dc0196ccaa
Gabriel Song <gabsong4@gmail.com>
2019-11-03 23:53:33 -0800

    Extra credit (not done)

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -3,13 +3,13 @@ var _; // globals
 describe("About Applying What We Have Learnt", function() {
   var products;
 
-  beforeEach(function () {
+  beforeEach(function() {
     products = [
-       { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
-       { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
-       { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
-       { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
-       { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+      { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+      { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+      { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+      { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+      { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
     ];
   });
 
@@ -61,7 +61,7 @@ describe("About Applying What We Have Learnt", function() {
     expect(sum).toBe(233168);
   });
 
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function() {
     var sum = _.chain(_.range(1000))
       .filter(num => num % 3 === 0 || num % 5 === 0)
       .reduce((sum, num) => sum + num, 0)
@@ -108,24 +108,18 @@ describe("About Applying What We Have Learnt", function() {
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-
-
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-
   });
 
   it("should find the 10001st prime", function () {
-
   });
   */
 });
